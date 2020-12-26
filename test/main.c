42533c5cf871cf4fbfe9340619eb56efb71dfa5c
Thierry Seegers <surfintheusa@yahoo.com>
2020-12-26 14:45:56 +0100

    Further remove parentheses from TEST macros.

--- test/main.c
+++ test/main.c
@@ -83,8 +83,8 @@ int main()
         TEST(v0.element_size == sizeof(int));
 
         // Tests avec l'interface.
-        TEST((size(v0) == 0));
-        TEST((capacity(v0) == 0));
+        TEST(size(v0) == 0);
+        TEST(capacity(v0) == 0);
         
         destroy(&v0);
 
@@ -93,8 +93,8 @@ int main()
 
         TEST(v1.data != NULL);
         TEST(v1.element_size == sizeof(int));
-        TEST((size(v1) == 1));
-        TEST((capacity(v1) == 1));
+        TEST(size(v1) == 1);
+        TEST(capacity(v1) == 1);
 
         destroy(&v1);
 
@@ -102,8 +102,8 @@ int main()
 
         TEST(v10.data != NULL);
         TEST(v10.element_size == sizeof(int));
-        TEST((size(v10) == 10));
-        TEST((capacity(v10) == 10));
+        TEST(size(v10) == 10);
+        TEST(capacity(v10) == 10);
 
         destroy(&v10);
     }
@@ -119,49 +119,49 @@ int main()
         iterator const b0 = begin(&v0);
         iterator const e0 = end(&v0);
         TEST(b0.element == e0.element);
-        TEST((compare(b0, e0) == 0));
+        TEST(compare(b0, e0) == 0);
 
 
         iterator b1 = begin(&v1);
         iterator const e1 = end(&v1);
-        TEST((b1.element + size(v1) * sizeof(int) == e1.element));
-        TEST((compare(b1, e1) < 0));
-        TEST((compare(e1, b1) > 0));
+        TEST(b1.element + size(v1) * sizeof(int) == e1.element);
+        TEST(compare(b1, e1) < 0);
+        TEST(compare(e1, b1) > 0);
 
         increment(&b1, 1);
-        TEST((compare(b1, e1) == 0));
+        TEST(compare(b1, e1) == 0);
         decrement(&b1, 1);
-        TEST((compare(b1, e1) < 0));
+        TEST(compare(b1, e1) < 0);
 
         iterator i = at(&v1, 0);
-        TEST((compare(i, b1) == 0));
+        TEST(compare(i, b1) == 0);
         increment(&i, 1);
-        TEST((compare(i, e1) == 0));
+        TEST(compare(i, e1) == 0);
         decrement(&i, 1);
-        TEST((compare(i, b1) == 0));
+        TEST(compare(i, b1) == 0);
 
 
         iterator b10 = begin(&v10);
         iterator const e10 = end(&v10);
-        TEST((b10.element + size(v10) * sizeof(int) == e10.element));
-        TEST((compare(b10, e10) < 0));
-        TEST((compare(e10, b10) > 0));
+        TEST(b10.element + size(v10) * sizeof(int) == e10.element);
+        TEST(compare(b10, e10) < 0);
+        TEST(compare(e10, b10) > 0);
 
         increment(&b10, 1);
-        TEST((compare(b10, e10) < 0));
+        TEST(compare(b10, e10) < 0);
         increment(&b10, 1);
-        TEST((compare(b10, e10) < 0));
+        TEST(compare(b10, e10) < 0);
         increment(&b10, 8);
-        TEST((compare(b10, e10) == 0));
+        TEST(compare(b10, e10) == 0);
 
         i = at(&v10, 0);
         iterator const i1 = at(&v10, 1);
         iterator const i9 = at(&v10, 9);
-        TEST((compare(i, i1) < 0));
+        TEST(compare(i, i1) < 0);
         increment(&i, 1);
-        TEST((compare(i, i1) == 0));
+        TEST(compare(i, i1) == 0);
         increment(&i, 8);
-        TEST((compare(i, i9) == 0));
+        TEST(compare(i, i9) == 0);
 
 
         destroy(&v0);
@@ -175,13 +175,13 @@ int main()
 
         int const n22 = 22;
         push_back(&v, &n22);
-        TEST((size(v) == 1));
-        TEST((capacity(v) == 1));
+        TEST(size(v) == 1);
+        TEST(capacity(v) == 1);
 
         int r = *(int*)pop_back(&v);
         TEST(r == n22);
-        TEST((size(v) == 0));
-        TEST((capacity(v) == 1));
+        TEST(size(v) == 0);
+        TEST(capacity(v) == 1);
 
 
         int const n33 = 33, n0 = 0;
@@ -190,8 +190,8 @@ int main()
         push_back(&v, &n0);
         push_back(&v, &n0);
         push_back(&v, &n0);
-        TEST((size(v) == 5));
-        TEST((capacity(v) == 8));
+        TEST(size(v) == 5);
+        TEST(capacity(v) == 8);
 
         pop_back(&v);
         pop_back(&v);
@@ -199,8 +199,8 @@ int main()
         pop_back(&v);
         r = *(int*)pop_back(&v);
         TEST(r == n33);
-        TEST((size(v) == 0));
-        TEST((capacity(v) == 8));
+        TEST(size(v) == 0);
+        TEST(capacity(v) == 8);
 
         destroy(&v);
     }
@@ -213,21 +213,21 @@ int main()
         for(int n = 4; n >= 0; --n)
         {
             insert(&v, begin(&v), &n);
-            TEST((size(v) == 5 - n));
+            TEST(size(v) == 5 - n);
         }
 
         // Teste que les valeurs sont les bonnes.
         for(int n = 0; n != 5; ++n)
         {
-            TEST((*(int*)value(at(&v, n)) == n));
+            TEST(*(int*)value(at(&v, n)) == n);
         }
 
         // Efface une à une les valeurs en commencant du début.
         for(int n = 0; n != 5; ++n)
         {
-            TEST((*(int*)value(begin(&v)) == n));
+            TEST(*(int*)value(begin(&v)) == n);
             erase(&v, begin(&v));
-            TEST((size(v) == 4 - n));
+            TEST(size(v) == 4 - n);
         }
 
         // À la fin de cette boucle, le vecteur sera [0, 1, 2, 3, 4].
@@ -235,20 +235,20 @@ int main()
         {
             insert(&v, end(&v), &n);
         }
-        TEST((size(v) == 5));
+        TEST(size(v) == 5);
 
         // Efface aléatoirement certains éléments.
         erase(&v, at(&v, 2));   // Il nous reste : [0, 1, 3, 4]
-        TEST((*(int*)value(at(&v, 2)) == 3));
-        TEST((size(v) == 4));
+        TEST(*(int*)value(at(&v, 2)) == 3);
+        TEST(size(v) == 4);
 
         erase(&v, at(&v, 0));   // Il nous reste : [1, 3, 4]
         TEST(*(int*)value(begin(&v)) == 1);
-        TEST((size(v) == 3));
+        TEST(size(v) == 3);
 
         erase(&v, at(&v, 0));   // Il nous reste : [3, 4]
         TEST(*(int*)value(at(&v, 1)) == 4);
-        TEST((size(v) == 2));
+        TEST(size(v) == 2);
 
         destroy(&v);
     }
@@ -262,7 +262,7 @@ int main()
         for(iterator i = begin(&v), e = end(&v); compare(i, e) != 0; increment(&i, 1))
         {
             set(i, &n);
-            TEST((*(int*)value(i) == n));
+            TEST(*(int*)value(i) == n);
             ++n;
         }
 
@@ -271,41 +271,41 @@ int main()
         assign(&v_copie, begin(&v), end(&v));
 
         // Teste que les valeurs sont les bonnes.
-        TEST((size(v_copie) == 5));
+        TEST(size(v_copie) == 5);
         for(int n = 0; n != 5; ++n)
         {
-            TEST((*(int*)value(at(&v_copie, n)) == n));
+            TEST(*(int*)value(at(&v_copie, n)) == n);
         }
 
         // Après 'clear', la taille est de zéro mais la capacité reste inchangée.
         size_t const old_capacity = capacity(v);
         clear(&v);  // v = []
-        TEST((size(v) == 0));
-        TEST((capacity(v) == old_capacity));
-        TEST((compare(begin(&v), end(&v)) == 0));
+        TEST(size(v) == 0);
+        TEST(capacity(v) == old_capacity);
+        TEST(compare(begin(&v), end(&v)) == 0);
 
         // Échange les deux vecteurs.
         swap(&v, &v_copie);     // v = [0, 1, 2, 3, 4], v_copie = []
-        TEST((size(v) == 5));
-        TEST((size(v_copie) == 0));
+        TEST(size(v) == 5);
+        TEST(size(v_copie) == 0);
 
         // Affecte les deux dernières valeurs de 'v' à 'v_copie'.
         iterator i = end(&v);
         decrement(&i, 2);
         assign(&v_copie, i, end(&v));
-        TEST((size(v_copie) == 2));
-        TEST((*(int*)value(at(&v_copie, 0)) == 3));
-        TEST((*(int*)value(at(&v_copie, 1)) == 4));
+        TEST(size(v_copie) == 2);
+        TEST(*(int*)value(at(&v_copie, 0)) == 3);
+        TEST(*(int*)value(at(&v_copie, 1)) == 4);
         // Et 'v' reste inchangé.
-        TEST((size(v) == 5));
-        TEST((*(int*)value(at(&v, 0)) == 0));
-        TEST((*(int*)value(at(&v, 1)) == 1));
+        TEST(size(v) == 5);
+        TEST(*(int*)value(at(&v, 0)) == 0);
+        TEST(*(int*)value(at(&v, 1)) == 1);
 
         // 'swap' sur un même vecteur ne doit avoir aucun effet.
         swap(&v_copie, &v_copie);
-        TEST((size(v_copie) == 2));
-        TEST((*(int*)value(at(&v_copie, 0)) == 3));
-        TEST((*(int*)value(at(&v_copie, 1)) == 4));
+        TEST(size(v_copie) == 2);
+        TEST(*(int*)value(at(&v_copie, 0)) == 3);
+        TEST(*(int*)value(at(&v_copie, 1)) == 4);
 
         destroy(&v);
         destroy(&v_copie);
@@ -316,47 +316,47 @@ int main()
         vector v = make_vector(sizeof(int), 0, growth_factor_doubling);
 
         reserve(&v, 10);
-        TEST((size(v) == 0));
-        TEST((capacity(v) == 10));
+        TEST(size(v) == 0);
+        TEST(capacity(v) == 10);
 
         shrink_to_fit(&v);
-        TEST((size(v) == 0));
-        TEST((capacity(v) == 0));
+        TEST(size(v) == 0);
+        TEST(capacity(v) == 0);
 
         reserve(&v, 10);
         reserve(&v, 3);
-        TEST((size(v) == 0));
-        TEST((capacity(v) == 10));
+        TEST(size(v) == 0);
+        TEST(capacity(v) == 10);
 
         resize(&v, 10);
-        TEST((size(v) == 10));
-        TEST((capacity(v) == 10));
+        TEST(size(v) == 10);
+        TEST(capacity(v) == 10);
 
         int n = 22;
         push_back(&v, &n);
-        TEST((size(v) == 11));
-        TEST((capacity(v) == 20));
+        TEST(size(v) == 11);
+        TEST(capacity(v) == 20);
 
         shrink_to_fit(&v);
-        TEST((size(v) == 11));
-        TEST((capacity(v) == 11));
+        TEST(size(v) == 11);
+        TEST(capacity(v) == 11);
 
         for(n = 1; n != 4; ++n)
         {
             set(at(&v, n - 1), &n);
         }
         resize(&v, 3);
-        TEST((size(v) == 3));
-        TEST((capacity(v) == 11));
+        TEST(size(v) == 3);
+        TEST(capacity(v) == 11);
 
         for(n = 1; n != 4; ++n)
         {
-            TEST((*(int*)value(at(&v, n - 1)) == n));
+            TEST(*(int*)value(at(&v, n - 1)) == n);
         }
 
         shrink_to_fit(&v);
-        TEST((size(v) == 3));
-        TEST((capacity(v) == 3));
+        TEST(size(v) == 3);
+        TEST(capacity(v) == 3);
 
         destroy(&v);
     }
@@ -387,19 +387,19 @@ int main()
         }
 
         // La liste devrait contenir cinq villes.
-        TEST((size(silicon_valley_peninsula) == 5));
+        TEST(size(silicon_valley_peninsula) == 5);
 
         // On vérifie les infos de la première ville.
         ville_silicon_valley const* b = (ville_silicon_valley*)(begin(&silicon_valley_peninsula).element);
         TEST(b->index == 1);
-        TEST((strcmp(b->nom, "Redwood City") == 0));
-        TEST((strcmp(b->code_postal, "94063") == 0));
+        TEST(strcmp(b->nom, "Redwood City") == 0);
+        TEST(strcmp(b->code_postal, "94063") == 0);
 
         // On sait que la quatrième ville est incorrecte. Supprimons-la.
         erase(&silicon_valley_peninsula, at(&silicon_valley_peninsula, 3));
 
         // Le vecteur ne devrait plus contenir que quatre villes.
-        TEST((size(silicon_valley_peninsula) == 4));
+        TEST(size(silicon_valley_peninsula) == 4);
 
         // Ajoutons la ville manquante.
         ville_silicon_valley sunnyvale;
@@ -409,7 +409,7 @@ int main()
         push_back(&silicon_valley_peninsula, &sunnyvale);
 
         // La vecteur devrait contenir cinq villes.
-        TEST((size(silicon_valley_peninsula) == 5));
+        TEST(size(silicon_valley_peninsula) == 5);
 
         // Écriture du vecteur vers un fichier.
         FILE *db_modifiee = fopen("db_modifiee.txt", "w");
