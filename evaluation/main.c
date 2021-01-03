3fb67636a41ac61283ba467873e2aed671a13147
Thierry Seegers <surfintheusa@yahoo.com>
2021-01-03 18:18:54 +0100

    Use test_harness subtree.
    Remove all test related functionalities that are now found in test_harness.h.

--- evaluation/main.c
+++ evaluation/main.c
@@ -1,77 +1,17 @@
 #include "vector.h"
 
-#include <signal.h>
+#include "test_harness/test_harness.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// Nombre total de tests.
+// Valeurs pour le harnais de test spécifiques à ce programme.
 int const tests_total = 124;
-
-// Nombre total de tests exécutés. 
-int tests_executed = 0;
-
-// Pour chaque test qui réussi, cette variable sera incrémentée de 1.
-// Le but est de la garder égale à test_executes.
-int tests_successful = 0;
-
-// Incrémente le nombre de test exécutés de 1.
-// Si le test réussi, incrémente le nombre de tests réussis de 1.
-#define TEST(x) printf("%s:%d:0 %*s : ", __FILE__, __LINE__, __LINE__ < 100 ? -41 : -40, #x); \
-                tests_executed += 1;        \
-                if(x)                       \
-                {                           \
-                    tests_successful += 1;  \
-                    printf("[SUCCES]\n");   \
-                }                           \
-                else                        \
-                {                           \
-                    printf("[ECHEC]\n");    \
-                }
-
-// Incrémente le nombre de test exécutés de 1.
-// Compare le contenu de deux fichiers aux chemins a et b avec la commande diff.
-// Si les fichiers sont pareils, incrémente le nombre de tests réussis de 1.
-#define TEST_FILE(a, b) printf("%s:%d:0 %s %s %s : ", __FILE__, __LINE__, "diff --text --strip-trailing-cr", a, b);   \
-                        tests_executed += 1;            \
-                        {                               \
-                            int const r = system("diff --text --strip-trailing-cr " a " " b " > /dev/null");    \
-                            if(!WEXITSTATUS(r))         \
-                            {                           \
-                                tests_successful += 1;  \
-                                printf("[SUCCES]\n");   \
-                            }                           \
-                            else                        \
-                            {                           \
-                                printf("[ECHEC]\n");    \
-                            }                           \
-                        }
-
-// Affiche le sommaire des résultats des tests.
-void print_summary()
-{
-    printf("---\n%-20s : %d\n%-20s : %3d\n%-20s : %3d\n", "Nombre de tests", tests_total, "Tests executes", tests_executed, "Tests reussis", tests_successful);
-}
-
-// Fonction à executer lors d'une segmentation fault.
-// On imprime les résultats obtenus jusqu'à lors et on arrête immédiatement le programme.
-void segfault_sigaction(int signal, siginfo_t *si, void *arg)
-{
-    printf("[SEGFAULT]\n");
-    print_summary();
-    exit(tests_executed - tests_successful);
-}
+int const test_column_width = 40;
 
 int main()
 {
-    // Mise en place de la fonction à exécuter lors d'une segmentation fault.
-    struct sigaction sa;
-    memset(&sa, 0, sizeof(struct sigaction));
-    sigemptyset(&sa.sa_mask);
-    sa.sa_sigaction = segfault_sigaction;
-    sa.sa_flags = SA_SIGINFO;
-    sigaction(SIGSEGV, &sa, NULL);
-
     float const growth_factor_doubling = 2.;
 
     // Tests de la fonction 'make_vector', 'size' et 'capacity'.
@@ -452,8 +392,6 @@ int main()
         // Comparaison de la BD modifiée par le code et de la BD comme elle devrait maintenent être.
         TEST_FILE("db_modifiee.txt", "test/db_corrigee.txt");
     }
-
-    print_summary();
 
     return tests_executed - tests_successful;
 }
